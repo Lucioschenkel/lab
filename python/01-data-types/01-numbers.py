2a637dd316088d1ed9c0fbb85480d1ebd87b7b06
Lucioschenkel <lucioless@gmail.com>
2024-12-18 18:53:20 -0300

    chore: python data types

---
+++ python/01-data-types/01-numbers.py
@@ -0,0 +1,22 @@
+# Integers
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+
+print(type(2 + 4))
+print(type(2 / 4)) # float
+
+print(2 ** 2) # Two to the power of 2
+
+print(2 // 4) # 0 -> Integer division
+
+print(5 // 4) # 1
+
+print(5 % 4) # 1 - Remainder of the integer division
+
+# math functions
+print(round(3.1)) # 3
+print(round(3.9)) # 4
+
+print(abs(-5)) # 5
