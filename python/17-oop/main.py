185cf0620a024a91211da29e480d722ee78518a1
Lucioschenkel <39536631+Lucioschenkel@users.noreply.github.com>
2026-02-09 13:35:45 -0300

    feat: more python snippets

--- python/17-oop/main.py
+++ python/17-oop/main.py
@@ -9,6 +9,10 @@ class PlayerCharacter:
             # Attributes
             self.name = name
             self.age = age
+            # Python does not support access modifiers
+            # By convention, users should treat attributes and methods 
+            # prefixed by an underscore as private
+            self._address = "Secret hideout"
 
     # Method
     def run(self):
@@ -18,6 +22,14 @@ class PlayerCharacter:
     def shout(self):
         print(f'My name is {self.name}')
 
+    @classmethod
+    def adding_things(cls, num1, num2): # cls is the class, in this case 'PlayerCharacter'
+        return num1 + num2
+
+    @staticmethod
+    def adding_things2(num1, num2): # Unlike @classmethod, we don't have access to the class
+        return num1 + num2
+
 player1 = PlayerCharacter('John', 30)
 
 print(player1.membership)
@@ -25,3 +37,7 @@ print(PlayerCharacter.membership)
 print(player1.name)
 
 player1.shout()
+
+# print(player1.adding_things(2,3))
+print(PlayerCharacter.adding_things(2,3))
+print(PlayerCharacter.adding_things2(2, 3))
