47da5bc062a89b0de29e87cf87b576cf1ed146f2
Lucioschenkel <lucio.schenkel@proton.me>
2026-02-16 15:58:12 -0300

    feat: python test setup func

--- python/39-unit-tests/test.py
+++ python/39-unit-tests/test.py
@@ -2,10 +2,14 @@ import unittest
 import main
 
 class TestMain(unittest.TestCase):
+    def setUp(self) -> None:
+        # runs before each test, like "beforeEach" in Jest and Vitest
+        print('about to test a function')
+        return super().setUp()
     def test_do_stuff(self):
         num = 10
         result = main.do_stuff(num)
         self.assertEqual(result, 15)
 
-
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
