cec0f15f1348ada80822318954b545d6a1ade173
Lucioschenkel <39536631+Lucioschenkel@users.noreply.github.com>
2026-02-19 06:15:51 -0300

    feat: python servers with flask

---
+++ python/45-web-server/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(f'{page_name}.html')
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('thankyou')
+    else:
+        return 'Something went wrong.'
+
